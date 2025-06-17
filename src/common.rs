87a6fad4613b5877ce446f3350c3aa6867ad337f
oscar1209 <czc47@126.com>
2025-06-17 16:33:14 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -1004,7 +1004,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://42.193.130.54:21114".to_owned()
 }
 
 #[inline]
